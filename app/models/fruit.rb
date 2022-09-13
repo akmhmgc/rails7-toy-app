33868644b047715773492a24fe72e73efdce5e76
akmhmgc <akimuhamaguchi@icloud.com>
2022-09-13 09:04:50 +0900

    Add callback

--- app/models/fruit.rb
+++ app/models/fruit.rb
@@ -1,3 +1,8 @@
 class Fruit < ApplicationRecord
+  # after_initializeの場合、Fruit.find(3)などでFruitクラスのインスタンスが返された場合でも実行する
+  after_initialize :hoge
   scope :red, -> { p 'a' }
+  def hoge
+    puts 'hello'
+  end
 end
