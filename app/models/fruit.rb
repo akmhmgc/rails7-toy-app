92e95e23cd2c82869f0d99700b6e52967b43bc30
akmhmgc <akimuhamaguchi@icloud.com>
2022-09-16 09:13:00 +0900

    Add emthod

--- app/models/fruit.rb
+++ app/models/fruit.rb
@@ -1,8 +1,5 @@
 class Fruit < ApplicationRecord
-  # after_initializeの場合、Fruit.find(3)などでFruitクラスのインスタンスが返された場合でも実行する
-  after_initialize :hoge
-  scope :red, -> { p 'a' }
   def hoge
-    puts 'hello'
+    'hogehoge'
   end
 end
