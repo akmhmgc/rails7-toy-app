dee3f143f1efa04e13ade98bc3af14cf3efaf7e3
akmhmgc <akimuhamaguchi@icloud.com>
2022-09-17 12:27:27 +0900

    Apply validator

--- app/models/fruit.rb
+++ app/models/fruit.rb
@@ -1,8 +1,11 @@
 class Fruit < ApplicationRecord
+  validates :name , name: true
   # デフォルトとしてブロックを渡すと毎回評価された値が入る
   attribute :name, :string, default: proc{rand(10) }
 
   def hoge
     'hogehoge'
   end
+  
+  # def attributes
 end
