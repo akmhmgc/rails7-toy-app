6091c1b9d95cba949d2fdd382c7a448dec63d5af
akmhmgc <akimuhamaguchi@icloud.com>
2022-09-18 15:24:52 +0900

    Add reference_iod

--- db/migrate/20220918060446_add_customer_refto_posts.rb
+++ db/migrate/20220918060446_add_customer_refto_posts.rb
@@ -1,5 +1,6 @@
 class AddCustomerReftoPosts < ActiveRecord::Migration[7.0]
   def change
+    # foreign_key: 参照元のidの値のみを正しい値とする制約をつけるための設定
     add_reference :posts, :customer, foreign_key: true
   end
 end
